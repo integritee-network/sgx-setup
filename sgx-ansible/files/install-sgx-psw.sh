405bfca0505dbd92771d493efaf9c7d780d3db4e
BignaH <bigna.h@hotmail.com>
2021-12-03 09:05:15 +0100

    fix comment

--- sgx-ansible/files/install-sgx-psw.sh
+++ sgx-ansible/files/install-sgx-psw.sh
@@ -2,17 +2,17 @@
 linux_distro_name="$1"
 set -ex
 
-# build the psw installers
+# Build the psw installers.
 source linux/installer/bin/sgxsdk/environment
 sudo make deb_psw_pkg
 
-# make them available in apt
+# Make them available in apt.
 sudo make deb_local_repo
 sudo echo "deb [trusted=yes arch=amd64] file:${PWD}/linux/installer/deb/sgx_debian_local_repo ${linux_distro_name} main" | sudo tee /etc/apt/sources.list.d/intel-libsgx.list
 
 sudo apt update
-# remove in case something was deleted manually.. like intel folder.
-sudo apt remove libsgx-epid libsgx-urts libsgx-uae-service libsgx-launch libsgx-urts libsgx-epid libsgx-urts libsgx-quote-ex  libsgx-aesm-quote-ex-plugin libsgx-aesm-epid-plugin sgx-aesm-service -y
+# Remove installed packages to ensure clean installation process.
+sudo apt remove libsgx-epid libsgx-urts libsgx-uae-service libsgx-launch libsgx-urts libsgx-epid libsgx-urts libsgx-quote-ex libsgx-aesm-quote-ex-plugin libsgx-aesm-epid-plugin sgx-aesm-service -y
 sudo apt autoremove -y
-# install
-sudo apt install libsgx-epid libsgx-urts libsgx-uae-service libsgx-launch libsgx-urts libsgx-epid libsgx-urts libsgx-quote-ex  libsgx-aesm-quote-ex-plugin libsgx-aesm-epid-plugin sgx-aesm-service -y
+# Reinstall on clean state.
+sudo apt install libsgx-epid libsgx-urts libsgx-uae-service libsgx-launch libsgx-urts libsgx-epid libsgx-urts libsgx-quote-ex libsgx-aesm-quote-ex-plugin libsgx-aesm-epid-plugin sgx-aesm-service -y
